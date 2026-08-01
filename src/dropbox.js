295ef7eade75e5f045f295f2208051a9db23507a
aiyouaji <121384264+aiyouaji@users.noreply.github.com>
2026-08-01 22:30:24 +0900

    Update dropbox.js

--- src/dropbox.js
+++ src/dropbox.js
@@ -2,8 +2,7 @@
  * Dropbox APIと通信するためのヘルパーオブジェクト (V2 - アセット分離対応版)
  */
  window.dropboxApi = {
-    APP_KEY: '	
-vsahedh4r7gp3xs',
+    APP_KEY: 'vsahedh4r7gp3xs',
     METADATA_PATH: '/gemini_pwa_data.json',
     ASSETS_DIR_PATH: '/Gemini_PWA_Assets',
 
